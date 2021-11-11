694a86ce6a7674068a0023cf41527812e48e6e35
Logan Lossing <loganlossing@gmail.com>
2021-11-11 15:38:56 -0800

    Updating deletions

--- slopd_case_explorer/functions/filter_by_date.R
+++
@@ -1,9 +0,0 @@
-filter_by_date <- function (data, dates){
-    
-    print(dates)
-    
-    data %>%
-        filter(date >= as.POSIXct(dates[1]),   
-               date <= as.POSIXct(ymd(dates[2]) + 1))
-        
-}
--- slopd_case_explorer/functions/filter_by_search.R
+++
@@ -1,9 +0,0 @@
-filter_by_search <- function (data, search_pattern, search_column) {
-    data %>%
-        filter(
-            str_detect(
-                pattern = str_to_upper(search_pattern), 
-                {{search_column}}
-                )
-            )
-}
